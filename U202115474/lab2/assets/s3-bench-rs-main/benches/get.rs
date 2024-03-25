faec98563792842ac7e4605e071ce8cce83f4e8f
windflower <3336147249@qq.com>
2024-03-25 08:39:45 +0800

    feat:添加benchamrk

---
+++ U202115474/lab2/assets/s3-bench-rs-main/benches/get.rs
@@ -0,0 +1,35 @@
+//! Get 请求测试
+
+use criterion::async_executor::FuturesExecutor;
+use criterion::{criterion_group, criterion_main, Criterion};
+use reqwest::Url;
+use s3_bench_rs::{GetTaskBuilder, StdError, Task, TaskBuiler};
+
+const ENDPOINT: &str = "http://127.0.0.1:9000";
+const KEY: &str = "admin";
+const SECRET: &str = "password";
+const BUCKET: &str = "myminio";
+const OBJECT: &str = "wzy/minio.exe";
+#[tokio::main]
+async fn get() -> Result<String, Box<StdError>> {
+    let get_task_builder = GetTaskBuilder::new(
+        ENDPOINT.parse::<Url>().unwrap(),
+        KEY,
+        SECRET,
+        "minio",
+    );
+    let task = get_task_builder.spawn(BUCKET, OBJECT);
+    let text = task.run().await?;
+    Ok(text)
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("Async GetObject", move |b| {
+        b.to_async(FuturesExecutor).iter(|| async {
+            let _ret = get();
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
